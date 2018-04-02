e42c099e550dbc2bd0e95e37b8536961820d625a
cmcfadzean <court@kajabi.com>
2018-04-02 09:04:59 -0700

    addding Relationship

--- app/models/hunt.js
+++ app/models/hunt.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   user: DS.attr('string'),
-  beer: DS.attr('string'),
+  beer: DS.belongsTo('beer'),
   rate: DS.attr('number'),
   note: DS.attr('string'),
   timestamp: DS.attr('number')
