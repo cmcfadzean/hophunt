8ea0bb2d23261c3daaa4c8661e5a1998c7c7d936
cmcfadzean <court@kajabi.com>
2018-04-01 18:30:07 -0700

    Returnsing Hunts On Beer Page

--- app/routes/beers.js
+++ app/routes/beers.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 
-export default Route.extend({
+export default Route.extend(FindQuery,{
   model: function() {
-    return this.store.findAll('beer');
+    return RSVP.hash({
+      beers: this.get('store').findAll('beer'),
+      hunts: this.get('store').findAll('hunt')
+    });
   }
 });
